4648a1f5808a2cf219e6b91cccbe8c418a90026b
aniket jadhav <aniket.jadhav.8151@gmail.com>
2019-05-17 02:40:55 +0530

    updated corresponding php and css to thsese files.  also tested image upload functionality.

--- personalInfo.php
+++ personalInfo.php
@@ -1,19 +1,89 @@
 <?php 
 
-  $fName = $_POST['Fname'];
-  $mName = $_POST['Mname'];
-  $lName = $_POST['Lname'];
-  $mobile = $_POST['mobile'];
-  $gender = $_POST['gender'];
-  $fName = $_POST['city'];
-  $fName = $_POST['age'];
-  $fName = $_POST['pincode'];
-  $profilePic = $_POST['profilePic'];
+  session_start();
+   $tempEmail = $_SESSION['email'];
 
-      $connection = mysqli_connect("localhost:3307", "root", "", "banking_db") or die("Connection FAILED!");
+ if(isset($_POST['submit'])) {
+    $fName = $_POST['Fname'];
+    $mName = $_POST['Mname'];
+    $lName = $_POST['Lname'];
+    $mobile = $_POST['mobile'];
+    
+    $city = $_POST['city'];
+    $age = $_POST['age'];
+    $pincode = $_POST['pinCode'];
+
+    if(!isset($_POST['submit'])){
+      $gender = $_POST['gender'];
+
+}
+ 
+  $connection = mysqli_connect("localhost:3307", "root", "", "banking_db") or die("Connection FAILED!");
+
+       $mobile_check_query = "SELECT * FROM bank_deatils";
+
+       $query_mobile = mysqli_query($connection, $mobile_check_query);
+
+      if(empty($fName)||empty($mName)||empty($lName)||empty($mobile)||empty($city)||empty($age)||empty($pincode)||empty($gender)){
+
+        echo "<h3 class = 'warning-msg'>Fileds can Not be Empty!</h3>";
+
+    }else if(strlen($mobile != 10)){
+          
+        "<h3 class = 'warning-msg'>Recheck your phone number!</h3>";
       
+    }else if(mysqli_num_rows($query_mobile) == true){
+   
+        echo "<h3 class = 'warning-msg'>This phone number is already used in the database!</h3>";
+      
+      }else if($age > 99){
 
+        echo "<h3 class = 'warning-msg'>Are you Captain America?</h3>";
+
+      }else if(strlen($pincode != 6)){
+    
+        "<h3 class = 'warning-msg'>pincode has to be of 6 characters only!</h3>";       
+      }
+
+
+
+//   $profilePic = $_FILES['image']['tmp_name'];
+
+// $connection = mysqli_connect("localhost:3307", "root", "", "banking_db");
+
+//   $filename = $_FILES['image']['name'];  
+//   $fileTmpName = $_FILES['image']['tmp_name']; 
+//   $folder = 'upload_images/';
+
+// move_uploaded_file($fileTmpName, $folder.$filename);
+
+// $insert_query = "INSERT INTO `photos_bank_db`(`image`, date) VALUES ('$filename', NOW())";
+// //   $sql = "INSERT INTO photos_bank_db(image) VALUES ('$filename')";
+//   mysqli_query($connection, $insert_query);
+
+// $select_query = "SELECT * FROM photos_bank_db WHERE id = 26";
+//   $query2 = mysqli_query($connection, $select_query);
+
+//       $data = mysqli_fetch_array($query2);
+
+//          echo "<img src='upload_images/".$data['image']."'>";
+    }
 ?>
+
+<style>
+  
+  .warning-msg{
+  text-align:center;  
+  color:red;  
+ position:absolute;
+  bottom:25px;
+  left:50%;
+  transform:translateX(-50%);
+  
+  }
+
+</style>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -32,7 +102,7 @@
          
          <div class="Form-Element">
         
-             <form action="personalInfo.php" method = "POST">
+             <form action="personalInfo.php" method = "POST" enctype="multipart/form-data">
             
                  <div class= "testDiv">First Name: &nbsp; <input type="text" name="Fname"></div> 
                  <br>
@@ -62,9 +132,9 @@
                       <div class= "pinField">PIN code:&nbsp;&nbsp;&nbsp;&nbsp;
                      &nbsp;<input type="number" name="pinCode"></div>
                   
-                     <div><p class="profile-name">Profle Picture: </p><input type="file" name="profilePic" id="fileToUpload"></div>
+                     <div><p class="profile-name">Profle Picture: </p><input type="file" name="image" id="fileToUpload"></div>
 
-                    <input type="submit" name="pincode" value="save">
+                    <input type="submit" name="submit" value="save">
 
 
                  
@@ -73,8 +143,10 @@
           </div>
          <div class="Notice-Board">
              <h4 class = "note">NOTICE</h4>
-             <p class = "warning-para"><span>*</span>All the fields are mandatory. Please fill out all the fields to proceed further.</p>
-         </div>   
+             <p class = "warning-para"><span>*</span>All the fields except 'profile picture' are mandatory. Please fill out all the fields to proceed further.</p>
+           
+         </div> 
+
      </div>
 
 
